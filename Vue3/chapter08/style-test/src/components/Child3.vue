89302054e06a4ce72caf588ce3f467198d7286c0
Minwoo Kwon <m2nuuu@gmail.com>
2025-03-26 13:05:23 +0900

    vue 컴포넌트 심화 관련 코드 추가 및 수정

---
+++ Vue3/chapter08/style-test/src/components/Child3.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="child"><h2>Child3</h2></div>
+</template>
+<script>
+export default { name: 'Child3' };
+</script>
+<style scoped>
+.child {
+  background-color: orange; /* Child2는 blue, Child3는 orange */
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
