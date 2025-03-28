40b4d7ecd23f7d982df91d0231e7c5f4cd4ffaa5
Minwoo Kwon <m2nuuu@gmail.com>
2025-03-28 16:38:40 +0900

    vue 라우팅 실습 심화(1)

---
+++ Vue3/chapter10/실습/router-test/src/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+    <h1>404 NOT FOUND</h1>
+  </div>
+</template>
--- Vue3/chapter10/실습/router-test/src/router/index.js
+++ Vue3/chapter10/실습/router-test/src/router/index.js
@@ -6,14 +6,32 @@ import Members from '@/pages/Members.vue';
 import Videos from '@/pages/Videos.vue';
 import MemberInfo from '@/pages/MemberInfo.vue';
 
+import NotFound from '@/pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/about', name: 'About', component: About },
     { path: '/members', name: 'Members', component: Members },
-    { path: '/members/:id', name: 'MemberInfo', component: MemberInfo },
+    {
+      path: '/members/:id',
+      name: 'MemberInfo',
+      component: MemberInfo,
+      beforeEnter: (to, from) => {
+        // members/:id 경로는 /members에서만 접근 가능
+        // console.log(from.name);
+        if (from.name !== 'Members') {
+          return false;
+        }
+      },
+    },
     { path: '/videos', name: 'Videos', component: Videos },
+    {
+      path: '/:paths(.*)*', // 모든 매칭되지 않는 경로
+      name: 'NotFound',
+      component: NotFound,
+    },
   ],
 });
 export default router;
