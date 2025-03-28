14951b97d6fd78e88b18188171b1fea1ff2ce1af
Minwoo Kwon <m2nuuu@gmail.com>
2025-03-28 16:24:28 +0900

    vue 라우팅 실습 기본

---
+++ Vue3/chapter10/실습_기본/router-test/src/pages/MemberInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card card-body">
+    <div>
+      <!-- 경로 패턴: /members/:id<br /> -->
+      <!-- 요청 경로: {{ currentRoute.fullPath }}<br /> -->
+      <!-- id값: {{ currentRoute.params.id }}<br /> -->
+      <!-- {{ member[currentRoute.params.id] }} -->
+      <img :src="member[currentRoute.params.id].photo" />
+      <!-- <h3>{{ member[currentRoute.params.id] }}</h3> -->
+      <h3>{{ member[currentRoute.params.id].name }}</h3>
+      <p>{{ member[currentRoute.params.id].role }}</p>
+      <a
+        v-if="member[currentRoute.params.id].insta != 0"
+        :href="member[currentRoute.params.id].insta"
+        class="fa fa-instagram"
+      ></a>
+      <a
+        v-if="member[currentRoute.params.id].facebook != 0"
+        :href="member[currentRoute.params.id].facebook"
+        class="fa fa-facebook"
+      ></a>
+      <a
+        v-if="member[currentRoute.params.id].youtube != 0"
+        :href="member[currentRoute.params.id].youtube"
+        class="fa fa-youtube"
+      >
+      </a>
+      <br />
+      <button class="btn btn-link" @click.stop="goBackMemberList">
+        멤버 목록으로
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+import { useRoute, useRouter } from 'vue-router';
+import member from '@/members.json';
+
+export default {
+  name: 'MemberInfo',
+  setup() {
+    const currentRoute = useRoute();
+
+    const router = useRouter();
+    const goBackMemberList = () => {
+      router.push('/members');
+    };
+
+    return { currentRoute, member, goBackMemberList };
+  },
+};
+</script>
---
+++ Vue3/chapter10/실습_기본/router-test/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home from '@/pages/Home.vue';
+import About from '@/pages/About.vue';
+import Members from '@/pages/Members.vue';
+import Videos from '@/pages/Videos.vue';
+import MemberInfo from '@/pages/MemberInfo.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/about', name: 'About', component: About },
+    { path: '/members', name: 'Members', component: Members },
+    { path: '/members/:id', name: 'MemberInfo', component: MemberInfo },
+    { path: '/videos', name: 'Videos', component: Videos },
+  ],
+});
+export default router;
